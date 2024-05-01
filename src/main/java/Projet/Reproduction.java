9d523c07da94aacc072ee9a79ef01263d7d2e58a
kerAymen <158388179+kerAymen@users.noreply.github.com>
2024-05-01 02:49:17 -0400

    Update Reproduction.java

--- src/main/java/Projet/Reproduction.java
+++ src/main/java/Projet/Reproduction.java
@@ -1,4 +1,24 @@
 package Projet;
 
 public class Reproduction {
+  
+  public void reproduire(Animal[], int nbrMaximum) {
+    Animal[] animaux = new ArrayList 
+    for (Animaux animal: animaux) {
+
+       if (animal.age > 2){
+                animalAdulte++;
+            }
+
+
+      
+    if ( Animal.estAdulte()) {
+      Animaux
+    }
+
+  }   
+  }
+
+
+  
 }
